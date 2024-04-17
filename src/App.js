557188ce580f37ef815c5700b88f7915eb2b4738
Ashiko <alexandre.raguin49@gmail.com>
2024-04-17 15:16:01 +0200

    modified:   src/App.js 	deleted:    src/components/counter.js 	new file:   src/components/filtres.js 	new file:   src/components/form.js 	new file:   src/components/todo.js 	modified:   src/index.css 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,90 @@
-import React from 'react';
-import Counter from './components/counter';
+import { useState, useRef, useEffect } from "react";
+import { nanoid } from "nanoid";
 
-function App() {
+import Form from "./components/form";
+import Filtres from "./components/filtres";
+import Todo from "./components/todo";
+
+const FILTER_MAP = {
+  "Toutes": () => true,
+  "En cours": (task) => !task.completed,
+  "Realisées": (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("Toutes");
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+
+  const filterList = FILTER_NAMES.map((name) => (
+    <Filtres key={name} name={name} isPressed={name === filter} setFilter={setFilter} />
+  ));
+  
+  const tasksWords =
+    taskList.length !== 1 ? "tâches restantes" : "tâche restante";
+  const headingText = `${taskList.length} ${tasksWords}`;
+
+  function addTask(name) {
+    const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }  
+  
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const listHeadingRef = useRef(null);
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <Counter />
+    <div className="todoapp stack-large">
+      <h1>ToDo List</h1>
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {headingText}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const DATA = [
+  { id: "todo-0", name: "Se lever", completed: true },
+  { id: "todo-1", name: "Manger", completed: true },
+  { id: "todo-2", name: "Se préparer (douche + sac)", completed: false },
+  { id: "todo-3", name: "Prendre le bus (ligne n°3)", completed: false },
+  { id: "todo-4", name: "Faire ses devoirs", completed: false },
+  { id: "todo-5", name: "Faire une sieste", completed: true },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
